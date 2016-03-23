1ff49936c8f5a0fb8a28fdf91628cb768565b82a
Karthik Sirasanagandla <sirkarthik@gmail.com>
2016-03-24 00:01:18 +0530

    Invoking Activity's method from Fragment

--- src/com/example/fragmentsdemobasics/BasicFragment.java
+++ src/com/example/fragmentsdemobasics/BasicFragment.java
@@ -15,17 +15,18 @@ public class BasicFragment extends Fragment implements OnClickListener {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// Inflate the layout for this fragment.
 		// While there are simpler  flavors of the inflate() method on LayoutInflater,
-		// this one is required in case the ViewGroup happens to be a RelativeLayout, 
+		// this one is required in case the ViewGroup happens to be a RelativeLayout,
 		// so that we can process all of the positioning and sizing rules appropriately.
 		final View fragment = inflater.inflate(R.layout.fragment_basic, container, false);
-		
+
 		fragment.findViewById(R.id.btnFrag).setOnClickListener(this);
-		
+
 		return fragment;
 	}
 
 	@Override
 	public void onClick(View v) {
 		Log.i(TAG, "The button in Fragment is clicked");
+		((MainActivity) getActivity()).displayMessage();
 	}
 }
--- src/com/example/fragmentsdemobasics/MainActivity.java
+++ src/com/example/fragmentsdemobasics/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fragmentsdemobasics;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,4 +12,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
+	public void displayMessage() {
+		Toast.makeText(this, "I am invoked by some Fragment code..", Toast.LENGTH_LONG).show();
+	}
 }
