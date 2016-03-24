8b88d89cfbe2176e90dc83b2285e4368ccfe0e1e
Karthik Sirasanagandla <sirkarthik@gmail.com>
2016-03-24 22:41:54 +0530

    Over-ride Fragment's life-cycle methods to log the events

--- README.md
+++ README.md
@@ -11,16 +11,18 @@ In this project we are going to use a static fragment.
 Static fragments are fairly simple, once you have the Fragment implementation: just add the <fragment> element to where you want to have the fragment appear in your activity’s layout, such as our res/layout/activity_main.xml in our case in this project.
 
 ## Fragment's Life-cycle methods
-* onCreate()
+* onCreate(Bundle savedInstanceState)
+* onViewCreated() will be called after onCreateView(). This is particularly useful if you are inheriting the onCreateView() implementation but need to configure the resulting views, such as with a ListFragment and needing to set up an adapter.
+* onActivityCreated() will be called after onCreate() and onCreateView(), to indicate that the activity’s onCreate() has completed. If there is something that you need to initialize in your fragment that depends upon the activity’s onCreate() having completed its work, you can use onActivityCreated() for that initialization work.
+* onViewStateRestored(Bundle savedInstanceState)
 * onStart() (but not onRestart()) 
 * onResume()
 * onPause()
-*onStop()
+* onSavedInstanceState(Bundle outState)
+* onStop()
 * onDestroy()
 * onAttach() called when a fragment is attached to an activity
 * onCreateView()
-* onViewCreated() will be called after onCreateView(). This is particularly useful if you are inheriting the onCreateView() implementation but need to configure the resulting views, such as with a ListFragment and needing to set up an adapter.
-* onActivityCreated() will be called after onCreate() and onCreateView(), to indicate that the activity’s onCreate() has completed. If there is something that you need to initialize in your fragment that depends upon the activity’s onCreate() having completed its work, you can use onActivityCreated() for that initialization work.
 * onDestroyView() is called before onDestroy(). This is the counterpart to onCreateView() where you set up your UI. If there are things that you need to clean up specific to your UI, you might put that logic in onDestroyView().
 * onDetach() is called after onDestroy(), to let you know that your fragment has been disassociated from its hosting activity.
 
--- src/com/example/fragmentsdemobasics/BasicFragment.java
+++ src/com/example/fragmentsdemobasics/BasicFragment.java
@@ -1,6 +1,7 @@
 package com.example.fragmentsdemobasics;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,7 +10,6 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
 public class BasicFragment extends Fragment implements OnClickListener {
-	private static final String TAG = BasicFragment.class.getSimpleName();
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,7 +26,99 @@ public class BasicFragment extends Fragment implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		Log.i(TAG, "The button in Fragment is clicked");
+		Log.d(getClass().getSimpleName(), "The button in Fragment is clicked");
 		((MainActivity) getActivity()).displayMessage();
 	}
+
+
+	/****************************************************************
+	 *
+	 * Fragment Life-cycle methods below
+	 *
+	 ****************************************************************/
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Log.d(getClass().getSimpleName(), "In onActivityCreated(Bundle savedInstanceState)");
+	}
+
+	@Override
+	public void onAttach(Context context) {
+		// super.onAttach(context);
+		Log.d(getClass().getSimpleName(), "In onAttach(Context context)");
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.d(getClass().getSimpleName(), "In onCreate(Bundle savedInstanceState)");
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(getClass().getSimpleName(), "In onDestroy()");
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		Log.d(getClass().getSimpleName(), "In onDestroyView()");
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		Log.d(getClass().getSimpleName(), "In onDetach()");
+	}
+
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		Log.d(getClass().getSimpleName(), "In onLowMemory()");
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.d(getClass().getSimpleName(), "In onPause()");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d(getClass().getSimpleName(), "In onResume()");
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Log.d(getClass().getSimpleName(), "In onSaveInstanceState(Bundle outState)");
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d(getClass().getSimpleName(), "In onStart()");
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.d(getClass().getSimpleName(), "In onStop()");
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		Log.d(getClass().getSimpleName(), "In onViewCreated(View view, Bundle savedInstanceState)");
+	}
+
+	@Override
+	public void onViewStateRestored(Bundle savedInstanceState) {
+		super.onViewStateRestored(savedInstanceState);
+		Log.d(getClass().getSimpleName(), "In onViewStateRestored(Bundle savedInstanceState)");
+	}
+
 }
